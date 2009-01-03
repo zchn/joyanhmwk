34728948ebdb865ebcd5c0fb4db462302aec00b3
joyanlj <joyanlj@f886a5b4-c40a-11dd-9d6e-f9b09b1023f7>
2009-01-03 14:47:50 +0000

    for语句实现完成，注意没有实现break和continue语句。

--- cpp/test.c
+++ cpp/test.c
@@ -1,32 +1,8 @@
-/* ¿ØÖÆÓï¾ä-for */
-
 void main()
 {
   int i, j;
   for(i=1;i<=10;i=i+1)
+{
     printf(i);
-  println();
-  for(i=1;;i=i+1)
-  {
-    printf(i);
-    if(i >= 10) break;
-  }
-  println();
-  for(;i>=-10;i=i-1)
-  {
-    if(i > 0) continue;
-    printf(i);
-  }
-  println();
-  j = 0;
-println();
-  for(;;)
-  {
-    j = j + i % 2;
-    i = i / 2;
-	printf(i);
-    if(i == 0) break;  
-  }
-  printf(j);
-  println();
+}  println();
 }
