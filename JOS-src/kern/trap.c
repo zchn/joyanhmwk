76c4ef7f1495492ea8b21e185f50085e0118816a
joyanlj <joyanlj@f886a5b4-c40a-11dd-9d6e-f9b09b1023f7>
2009-04-12 13:22:28 +0000

    Lab3 complete except Challenge 2,3

--- JOS-src/kern/trap.c
+++ JOS-src/kern/trap.c
@@ -137,6 +137,16 @@ trap_dispatch(struct Trapframe *tf)
 	case T_PGFLT:
 		page_fault_handler(tf);
 		return;
+	case T_SYSCALL:
+		tf->tf_regs.reg_eax=syscall(
+			tf->tf_regs.reg_eax,
+			tf->tf_regs.reg_edx,
+			tf->tf_regs.reg_ecx,
+			tf->tf_regs.reg_ebx,
+			tf->tf_regs.reg_edi,
+			tf->tf_regs.reg_esi
+			);
+		return;
 	}
 
 	// Unexpected trap: The user process or the kernel has a bug.
@@ -185,6 +195,9 @@ page_fault_handler(struct Trapframe *tf)
 	// Handle kernel-mode page faults.
 	
 	// LAB 3: Your code here.
+	if((tf->tf_cs & 3) == 0){
+		panic("Page fault in Kernel!\n");
+	}
 
 	// We've already handled kernel-mode exceptions, so if we get here,
 	// the page fault happened in user mode.
